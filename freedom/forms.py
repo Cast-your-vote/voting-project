296816be12530180088aa8b616ac0f81fd85f668
Namshivrah <88289621+Namshivrah@users.noreply.github.com>
2024-04-09 11:35:02 +0300

    Create forms.py

---
+++ freedom/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ModelChoiceField, ModelForm
+from .models import Voters, Candidates
+#create forms here
+
+class VoterRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Voters
+        fields = ['first_name', 'middle_name', 'last_name', 'gender', 'date_of_birth', 'nin_number', 'phone_contact', 'voter_type', 'Polling_station']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+        }       
+
+class CandidateRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Candidates
+        fields = ['profile_pic','first_name', 'middle_name', 'last_name', 'gender', 'political_party', 'post_aspired_for']
