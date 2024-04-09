efbfc64ec0077a1f5c5288ec5904e176174a34fe
Namshivrah <88289621+Namshivrah@users.noreply.github.com>
2024-04-09 11:34:20 +0300

    Update models.py

--- freedom/models.py
+++ freedom/models.py
@@ -1,124 +1,170 @@
-from datetime import date
 from django.db import models
+import uuid
+from django.utils import timezone
 
-# Create your models here.
 
+# Model for storing Addresses
 class Address(models.Model):
-    village = models.CharField(max_length=30)
-    parish = models.CharField(max_length=30)
-    sub_county = models.CharField(max_length=30)
-    county = models.CharField(max_length=30)
-    district = models.CharField(max_length=30)
-    region = models.CharField(max_length=30)
-
+    village = models.CharField(max_length=100)
+    parish = models.CharField(max_length=100)
+    sub_county = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.parish
+    class Meta:
+        verbose_name_plural = '1. Addresses'
 
-class Political_party(models.Model):
-    name = models.CharField(max_length=30)
-    emblem = models.ImageField(upload_to='emblem_images/')
-    description = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-    
-class Voter_type(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
-    
-    PRIVELEDGE_CHOICES = [
-        ('FP', 'Fingerprint_patterns'),
-        ('VC', 'Voice'),
-        ('BR', 'Braille_keypad'),
-    ]
-
-    priveledges = models.CharField(max_length=2, choices=PRIVELEDGE_CHOICES)
-
-    def __str__(self):
-        return self.name
-
-class Audio(models.Model):
-    english_text = models.CharField(max_length=50)
-    luganda_text = models.CharField(max_length=50)
-    kiswahili_text = models.CharField(max_length=50)
-    english_audio = models.FileField(upload_to='english_audio/')
-    luganda_audio = models.FileField(upload_to='luganda_audio/')
-    kiswahili_audio = models.FileField(upload_to='kiswahili_audio/')
-    cast_voice = models.FileField(upload_to='cast_audio/')
-
-    def __str__(self):
-        return self.english_text
-
-class Polling_station(models.Model):
-    name = models.CharField(max_length=30)
+#Model For storing polling stations
+class PollingStation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    name = models.CharField(max_length=100)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
-
+    date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = '2. PollingStations'
 
-class Candidate(models.Model):
-    image = models.ImageField(upload_to='candidate_images/')
-    first_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    political_party = models.ForeignKey(Political_party, on_delete=models.CASCADE)
-    position = models.CharField(max_length=50)
 
+# Model For the Voters
+GENDERS = [
+    ('Male', 'Male'),
+    ('Female', 'Female')
+]
+VOTER_TYPES = [
+    ('Blind', 'Blind'),
+    ('Blind_and_Mute', 'Blind_and_Mute'),
+    ('Blind_and_Lame', 'Blind_and_Lame')
+]
+class Voters(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True,null=True)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=6, choices=GENDERS)
+    date_of_birth = models.DateField()
+    nin_number = models.CharField(max_length=100)
+    phone_contact = models.CharField(max_length=100)
+    voter_type = models.CharField(max_length=15, choices=VOTER_TYPES)
+    Polling_station = models.ForeignKey(PollingStation, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.first_name
-
-class Voter(models.Model):
-    first_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    gender = models.CharField(max_length=10)
-    date_of_birth = models.DateField(auto_now_add=True)
-    nin_number = models.CharField(max_length=15)
-    phone_contact = models.CharField(max_length=10)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    Voter_type = models.ForeignKey(Voter_type, on_delete=models.CASCADE)
-    #fingerprint_pattern = models.ForeignKey(Fingerprints, on_delete=models.CASCADE)
-    Polling_station = models.ForeignKey(Polling_station, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = '3. Voters'
     
 
+# Model For the Voter's Finger Prints
+HAND = [
+    ('Right_Hand', 'Right_Hand'),
+    ('Left_Hand', 'Left_Hand')
+]
+FINGER = [
+    ('Thumb', 'Thumb'),
+    ('Index Finger', 'Index Finger'),
+    ('Middle Finger', 'Middle Finger'),
+    ('Ring Finger', 'Ring Finger'),
+    ('Pinky Finger', 'Pinky Finger')
+]
+class FingerPrints(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    voter = models.OneToOneField(Voters, on_delete=models.CASCADE)
+    hand = models.CharField(max_length=10, choices=HAND)
+    finger = models.CharField(max_length=13, choices=FINGER)
+    fingerprint_image = models.BinaryField(default=b'')
+    date_created = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return self.right_thumb 
+    def __str__(self):
+        return f"{self.hand} - {self.finger} fingerprint for {self.voter}"
+
+
+    class Meta:
+        verbose_name_plural = '4. FingerPrints'
+    
+
+# Model for storing the audio types
+AUDIO_TYPES = [
+    ('English', 'English'),
+    ('Luganda', 'Luganda'),
+    ('Kiswahili', 'Kiswahili')
+]
+class AudioTypes(models.Model):
+    audio_type = models.CharField(max_length=10, choices=AUDIO_TYPES)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.audio_type
+    class Meta:
+        verbose_name_plural = '5. AudioTypes'
+
+
+# Model For Storing the audio files
+class Audios(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    audio_type = models.ForeignKey(AudioTypes, on_delete=models.CASCADE)
+    audio_text = models.CharField(max_length=200)
+    audio_file = models.FileField(upload_to='Audios')
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.audio_text
+    class Meta:
+        verbose_name_plural = '6. Audios'
+    
+
+# Model for storing the Candiates
+GENDERS = [
+    ('Male', 'Male'),
+    ('Female', 'Female')
+]
+POLITICAL_PARTY = [
+    ('NRM', 'NRM'),
+    ('FDC', 'FDC'),
+    ('NUP', 'NUP')
+]
+ASPIRING_POSTS = [
+    ('President', 'President'),
+    ('Member of Parliament', 'Member of Parliament'),
+    ('Chairperson LCV', 'Chairperson LCV'),
+]
+class Candidates(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    profile_pic = models.ImageField(upload_to='Candidates')
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10, choices=GENDERS)
+    political_party = models.CharField(max_length=100, choices=POLITICAL_PARTY)
+    post_aspired_for = models.CharField(max_length=100, choices=ASPIRING_POSTS)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
 
     def __str__(self):
-        return self.first_name
-    
-class Fingerprints(models.Model):
-    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
-    right_thumb = models.BinaryField()
-    right_index_finger = models.BinaryField()
-    right_middle_finger = models.BinaryField()
-    right_ring_finger = models.BinaryField()
-    right_pinky_finger = models.BinaryField()
-    left_thumb = models.BinaryField()
-    left_index_finger = models.BinaryField()
-    left_middle_finger = models.BinaryField()
-    left_ring_finger = models.BinaryField()
-    left_pinky_finger = models.BinaryField()
+        return self.full_name()
+    # def __str__(self):
+    #     return self.first_name
+    class Meta:
+        verbose_name_plural = '7. Candidates'
 
+
+# Model to store the casted votes [For tallying]
+class CastedVotes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    candidate = models.ForeignKey(Candidates, on_delete=models.CASCADE)
+    voter = models.ForeignKey(Voters, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.right_thumb
-    
+        casted_votes = "Casted Votes"
+        return casted_votes
+    class Meta:
+        verbose_name_plural = '8. CastedVotes'
 
 
-    
-    
-    
-    
-    
 
-   # class Meta:
-    #    verbose_name = _("")
-    #    verbose_name_plural = _("s")
-
-   # def __str__(self):
-   #     return self.name
-
-    #def get_absolute_url(self):
-    #    return reverse("_detail", kwargs={"pk": self.pk})
-#)
     
 
